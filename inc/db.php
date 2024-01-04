7e831a0b8e7c72d0ceae7a908b3a3e53974e7fc8
azzam-81 <155556629+azzam-81@users.noreply.github.com>
2024-01-04 03:31:08 +0300

    Add files via upload

---
+++ inc/db.php
@@ -0,0 +1,11 @@
+<?php
+
+ 
+
+$conn= mysqli_connect('localhost','root','root','win');
+
+if (!$conn){
+
+    echo 'error:' . mysqli_connect_error();
+
+}
---
+++ parts/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+<script src="./js/bootstrap.bundle.min.js"></script>
+
+<script src="./js/script.js"></script>
+
+<script src="./js/loader.js"></script>
+
+ 
+
+</body>
+
+</html>
